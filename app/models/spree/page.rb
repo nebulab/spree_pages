7ba97538892805a31a366e5ca894ced530b3c563
Daniele Palombo <daniele.palombo@gmail.com>
2017-05-11 17:48:54 +0200

    Move where filter inside lambda scope

--- app/models/spree/page.rb
+++ app/models/spree/page.rb
@@ -2,8 +2,8 @@ module Spree
   class Page < ActiveRecord::Base
     validates :title, :presence => true
     validates :permalink, :uniqueness => true
-  
-    scope :published, where(:published => true)
+
+    scope :published, ->{ where(:published => true) }
 
     paginates_per 50
   end
